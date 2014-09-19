0400837a57708f4002ed15e59ceef4b8a1b1dbb2
Nghia Nguyen <zinzinday@gmail.com>
2014-09-20 05:59:13 +0700

    Add sourcePath

--- Select2Asset.php
+++ Select2Asset.php
@@ -4,7 +4,7 @@ namespace yii\select2;
 
 class Select2Asset extends yii\web\AssetBundle {
 
-    public $sourcePath = '';
+    public $sourcePath = '@bower/select2';
     public $js = ['select2.js'];
     public $css = ['select2.css'];
 
