7a781431f5edc71de3ac1543f34d22172e64bdad
Nghia Nguyen <zinzinday@gmail.com>
2014-09-20 06:31:01 +0700

    Fix sourcePath

--- Select2BootstrapAsset.php
+++ Select2BootstrapAsset.php
@@ -4,7 +4,7 @@ namespace yii\select2;
 
 class Select2BootstrapAsset extends yii\web\AssetBundle {
 
-    public $sourcePath = '@bower/select2';
+    public $sourcePath = '@bower/select2-bootstrap-css';
     public $css = ['select2-bootstrap.css'];
     public $depends = ['yii\bootstrap\BootstrapAsset'];
 
