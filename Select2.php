bfeee504b5aa274f9e88619f827a586ba9270541
Nghia Nguyen <zinzinday@gmail.com>
2014-09-20 05:57:40 +0700

    Initial commit

---
+++ Select2.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace yii\select2;
+
+use yii\helpers\Html;
+use yii\web\AssetBundle;
+use yii\helpers\ArrayHelper;
+
+class Select2 extends \yii\widgets\InputWidget {
+
+    public $items = [];
+    public $clientOptions = [];
+    private $_assetBundle;
+
+    public function init()
+    {
+        if ($this->hasModel()) {
+            $this->options['id'] = Html::getInputId($this->model, $this->attribute);
+        } else {
+            $this->options['id'] = $this->getId();
+        }
+    }
+
+    public function run()
+    {
+        if ($this->hasModel()) {
+            echo Html::activeDropDownList($this->model, $this->attribute, $this->items, $this->options);
+        } else {
+            echo Html::dropDownList($this->name, $this->value, $this->items, $this->options);
+        }
+    }
+
+    public function registerAssetBundle()
+    {
+        $this->_assetBundle = Select2Asset::register($this->getView());
+    }
+
+    public function registerLocate()
+    {
+        $locate = ArrayHelper::getValue($this->clientOptions, 'locales', FALSE);
+        if ($locate) {
+            $locateAsset = 'select2_locale_' . $locate . '.js';
+            if (file_exists(Yii::getAlias($this->getAssetBundle()->sourcePath . '/' . $locateAsset))) {
+                $this->getAssetBundle()->js[] = $locateAsset;
+            } else {
+                ArrayHelper::remove($this->clientOptions, 'locales');
+            }
+        }
+    }
+
+    public function registerScript()
+    {
+        $clientOptions = (count($this->clientOptions)) ? Json::encode($this->clientOptions) : '';
+        $this->getView()->registerJs("jQuery('#{$this->options['id']}').select2({$clientOptions});");
+    }
+
+    public function getAssetBundle()
+    {
+        if (!($this->_assetBundle instanceof AssetBundle)) {
+            $this->registerAssetBundle();
+        }
+        return $this->_assetBundle;
+    }
+
+}
---
+++ Select2Asset.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace yii\select2;
+
+class Select2Asset extends yii\web\AssetBundle {
+
+    public $sourcePath = '';
+    public $js = ['select2.js'];
+    public $css = ['select2.css'];
+
+}
