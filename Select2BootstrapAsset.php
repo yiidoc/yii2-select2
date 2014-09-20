9f3996829967514c1c3318f7ecea9b3e68fb0028
Nghia Nguyen <zinzinday@gmail.com>
2014-09-21 06:27:47 +0700

    Change BootstrapAsset bower

--- Select2BootstrapAsset.php
+++ Select2BootstrapAsset.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace yii\select2;
+
 use yii\web\AssetBundle;
+
 class Select2BootstrapAsset extends AssetBundle {
 
-    public $sourcePath = '@bower/select2-bootstrap-css';
-    public $css = ['select2-bootstrap.css'];
+    public $sourcePath = '@bower/select2-bootstrap-css-bootstrap3';
+    public $css = ['dist/css/select2-bootstrap.css'];
     public $depends = ['yii\bootstrap\BootstrapAsset'];
 
 }
